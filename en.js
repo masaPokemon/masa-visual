c77cc49330646201241215a558bf0f829f2b75f8
masapokemon <masa.gongon2011@gmail.com>
2025-02-03 10:31:12 +0900

    first commit

---
+++ en.js
@@ -0,0 +1,159 @@
+Msg = {
+  categories: {
+    "html": "HTML",
+    "baseframe": "基本構造",
+    "textstructure": "テキスト構造",
+    "markup": "テキストマークアップ",
+    "style": "スタイル",
+    "enumerations": "列挙",
+    "tables": "表",
+    "forms": "フォーム",
+    "scripts": "スクリプト",
+    "scripting": "スクリプト処理",
+    "logic": "論理",
+    "loops": "ループ",
+    "math": "数学",
+    "text": "テキスト",
+    "lists": "リスト",
+    "colour": "色",
+    "variables": "変数",
+    "functions": "関数",
+  },
+  blocks: {
+    "baseframe": {
+      "message0": "ドキュメント %1 ヘッダー %2 %3 コンテンツ %4 %5",
+    },
+    "html": {
+      "message0": "ドキュメント %1 %2",
+    },
+    "body": {
+      "message0": "コンテンツ %1 %2",
+    },
+    "head": {
+      "message0": "ヘッダー %1 %2",
+    },
+    "title": {
+      "message0": "タイトル %1",
+    },
+    "paragraph": {
+      "message0": "段落 %1",
+    },
+    "plaintext": {
+      "message0": "テキスト %1",
+    },
+    "division": {
+      "message0": "区分 %1 %2",
+    },
+    "style": {
+      "message0": "スタイル =  %1 %2",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "color": {
+      "message0": "文字色 :  %1",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "bgcolour": {
+      "message0": "背景色 :  %1",
+      "helpUrl": ""
+    },
+    "genericstyle": {
+      "message0": "%1 : %2",
+      "tooltip": "",
+      "helpUrl": "",
+      "args0": [
+        {"text": "プロパティ"},
+        {"text": "値"}
+      ],
+    },
+    "generictag": {
+      "message0": "< %1 > %2 %3",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [{"text": "タグ"},],
+    },
+    "more_attributes": {
+      "message0": "%1 %2 %3",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "genericattribute": {
+      "message0": "%1  =  %2",
+      "tooltip": "",
+      "helpUrl": "",
+      "args0": [
+        {"text": "属性"},
+        {"text": "値"}
+      ],
+    },
+    "link": {
+      "message0": "%1 へのリンク %2 %3",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [{"text": "ターゲット"}],
+    },
+    "span": {
+      "message0": "スパン %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "image": {
+      "message0": "画像 %1 または %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [
+        {"text": "URL"},
+        {"text": "代替テキスト"}
+      ],
+    },
+    "emphasise": {
+      "message0": "強調 %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "strong": {
+      "message0": "重要 %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "headline": {
+      "message0": "見出し %1 %2 %3",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [{"options": [
+        ["レベル 1","h1"],
+        ["レベル 2","h2"],
+        ["レベル 3","h3"],
+        ["レベル 4","h4"],
+        ["レベル 5","h5"],
+        ["レベル 6","h6"]
+      ]}],
+    },
+    "linebreak": {
+      "message0": "改行",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "horizontalbreak": {
+      "message0": "区切り線",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "unorderedlist": {
+      "message0": "順不同リスト %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "orderedlist": {
+      "message0": "番号付きリスト %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "listelement": {
+      "message0": "リスト項目 %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+  }
+}
